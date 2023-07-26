60b58af3f574e7cf354c43d7d28288c0ef4892b3
YASH <iamyash337@gmail.com>
2023-07-26 19:41:54 +0530

    Time: 36 ms (5.47%), Space: 57 MB (33.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet())
+            if (entry.getValue() > 1)
+                return true;
+        return false;
+    }
+}
