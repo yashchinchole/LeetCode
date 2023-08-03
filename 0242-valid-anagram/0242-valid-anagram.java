62a496cae3631698abc540b2d74b9549013b36c6
YASH <iamyash337@gmail.com>
2023-08-03 17:33:08 +0530

    Time: 13 ms (33.96%), Space: 43.9 MB (28.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,25 @@ class Solution {
         if (s.length() != t.length())
             return false;
 
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
+        // char[] sChars = s.toCharArray();
+        // char[] tChars = t.toCharArray();
 
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
+        // Arrays.sort(sChars);
+        // Arrays.sort(tChars);
 
-        return Arrays.equals(sChars, tChars);
+        // return Arrays.equals(sChars, tChars);
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        for (char c : t.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) - 1);
+
+        for (int val : map.values())
+            if (val != 0)
+                return false;
+        return true;
     }
 }
