9472fa920ee8d6c29da4779282b7148a242093ee
YASH <iamyash337@gmail.com>
2023-08-03 13:44:03 +0530

    Time: 3 ms (93.64%), Space: 43.7 MB (49.36%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}
